c08d2b4e27dd5f2e32d26826c1780f2994efd676
zuiken-code <zuiken2022robo@gmail.com>
2026-03-17 01:12:19 +0900

    max scale

--- src/game/phaserGame.ts
+++ src/game/phaserGame.ts
@@ -4,9 +4,19 @@ import MainScene from "./scenes/MainScene";
 export const createGame = (parent: string) => {
   return new Phaser.Game({
     type: Phaser.AUTO,
+    // width/height は「基準となる解像度」として設定
     width: 400,
     height: 700,
     parent,
+    scale: {
+      // 画面全体にフィットさせる設定
+      mode: Phaser.Scale.FIT,
+      // 画面の中央に配置
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      // 親要素（div）の幅に合わせる
+      width: "100%",
+      height: "100%",
+    },
     physics: {
       default: "arcade",
       arcade: {
